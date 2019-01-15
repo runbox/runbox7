ffaf93092131fdd738e15a317803f1f5b9071c56
Peter Salomonsen <petersalomonsen@runbox.com>
2019-01-15 10:44:19 +0100

    compose: e2e test case for recipient validation #39

--- e2e/compose/compose.e2e-spec.ts
+++ e2e/compose/compose.e2e-spec.ts
@@ -24,5 +24,16 @@ describe('Compose', () => {
     browser.waitForAngularEnabled(false);
     expect(page.getDraftActionBarText()).toEqual('New message');
   });
+
+  it('should complain on invalid email address', async () => {
+    page.navigateTo();
+    await page.waitForRedirect();
+    browser.waitForAngularEnabled(false);
+    await page.setRecipientInputField('invalidaddress');
+    await page.waitForMailRecipientErrorInputToBePresent();
+    expect(page.getRecipientErrorText()).toEqual('Please enter a valid email address');
+    await page.setRecipientInputField('test@example.com');
+    await page.waitForMailRecipientErrorInputToBeAbsent();
+  });
 });
 
--- e2e/compose/compose.po.ts
+++ e2e/compose/compose.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element, until } from 'protractor';
+import { browser, by, element, until, Key } from 'protractor';
 
 export class ComposePage {
   navigateTo() {
@@ -11,5 +11,26 @@ export class ComposePage {
 
   getDraftActionBarText() {
     return element(by.css('mat-card-actions div')).getText();
+  }
+
+  async setRecipientInputField(value: string) {
+    const elm = element(by.css('mailrecipient-input input'));
+    await elm.sendKeys(value);
+    await elm.sendKeys(Key.TAB);
+  }
+
+  waitForMailRecipientErrorInputToBePresent() {
+    return browser.wait(until.elementLocated(by.css('mailrecipient-input mat-error')), 10000);
+  }
+
+  waitForMailRecipientErrorInputToBeAbsent() {
+    return browser.wait(
+      element(by.css('mailrecipient-input mat-error'))
+      .isPresent()
+      .then(present => !present), 10000);
+  }
+
+  getRecipientErrorText() {
+    return element(by.css('mailrecipient-input mat-error')).getText();
   }
 }
