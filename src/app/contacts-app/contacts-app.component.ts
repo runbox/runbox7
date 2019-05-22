1634e1bc53800f09362428308a65d9e9c3ab062c
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2019-05-22 17:31:35 +0200

    fix(Contacts): make sure we don't crash when searching unnamed contacts
    This also fixes display_name()'s signature to reflect that it returns a
    nullable string.

--- src/app/contacts-app/contacts-app.component.ts
+++ src/app/contacts-app/contacts-app.component.ts
@@ -84,7 +84,7 @@ export class ContactsAppComponent {
 
             return c.categories.find(g => g === target);
         }).filter(c => {
-            return c.display_name().toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1;
+            return c.display_name() && (c.display_name().toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1);
         });
         this.sortContacts();
     }
