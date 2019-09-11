0bd209681e1af2216a0ca0f03d767874f20bd0f7
Hernan Lopes <hernan604@runbox.com>
2019-09-11 02:32:06 +0200

    fix(dkim): remove unecessary variable that broke dkim select

--- src/app/dkim/dkim.component.ts
+++ src/app/dkim/dkim.component.ts
@@ -88,7 +88,6 @@ export class DkimComponent implements AfterViewInit {
   @Output() Close: EventEmitter<string> = new EventEmitter();
 
   dkim_domain;
-  dkim_domain_str;
   dkim_domains = [];
   domain;
   is_creating_keys = false;
@@ -153,7 +152,6 @@ export class DkimComponent implements AfterViewInit {
           this.dkim_domains = r.result.domains;
           const filtered = this.dkim_domains.filter((o) => o.domain === this.domain);
           this.dkim_domain = filtered[0];
-          this.dkim_domain_str = this.dkim_domain.domain;
           if ( ! this.dkim_domain ) {
             return setTimeout(() => { this.load_dkim_domains(); }, 5000);
           }
