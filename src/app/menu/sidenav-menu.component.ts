c02d5cdf978366cb3705de0254750d5f1793188c
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2019-11-01 16:56:48 +0100

    refactor(sidemenu): refactor the sidemenu into its own component
    This will allow us to use the same functionality in other sub-apps.

---
+++ src/app/menu/sidenav-menu.component.ts
@@ -0,0 +1,37 @@
+// --------- BEGIN RUNBOX LICENSE ---------
+// Copyright (C) 2016-2019 Runbox Solutions AS (runbox.com).
+//
+// This file is part of Runbox 7.
+//
+// Runbox 7 is free software: You can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// Runbox 7 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
+// ---------- END RUNBOX LICENSE ----------
+
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-sidenav-menu',
+    template: `
+<div class="sidenavMainMenu">
+    <div id="sidenavLogoContainer">
+        <a mat-button id="sidenavLogoButton" (click)="closeMenu.emit()">
+            <img src="assets/runbox7.png" id="logoSidenav" alt="Runbox 7" />
+        </a>
+        <br />
+    </div>
+</div>
+    `,
+})
+export class SidenavMenuComponent {
+    @Output() closeMenu = new EventEmitter<void>();
+}
