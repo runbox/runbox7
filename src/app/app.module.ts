d14169f6e21116042d16c4319f5ed1e8d5b39b9c
Hernan Lopes <hernan604@runbox.com>
2019-12-10 06:18:40 +0100

    fix(appmodule): removed unecessary imports from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -81,8 +81,6 @@ import { HotkeyModule } from 'angular2-hotkeys';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { DevComponent } from './dev/dev.component';
 import { RMM } from './rmm';
-import { ProfilesEditorModalComponent } from './profiles/profiles.editor.modal';
-import { AliasesEditorModalComponent } from './aliases/aliases.editor.modal';
 
 window.addEventListener('dragover', (event) => event.preventDefault());
 window.addEventListener('drop', (event) => event.preventDefault());
@@ -173,8 +171,6 @@ const routes: Routes = [
   ],
   bootstrap: [MainContainerComponent],
   entryComponents: [MoveMessageDialogComponent,
-    ProfilesEditorModalComponent,
-    AliasesEditorModalComponent,
   ]
 })
 export class AppModule { }
