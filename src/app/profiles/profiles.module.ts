9ae2eb139ef82a417ef58145caf3e5d19bbed3b1
Hernan Lopes <hernan604@runbox.com>
2019-12-12 13:16:42 +0100

    fix(identities): re-add required components to NgModule

--- src/app/profiles/profiles.module.ts
+++ src/app/profiles/profiles.module.ts
@@ -51,6 +51,8 @@ import { AliasesEditorModalComponent } from '../aliases/aliases.editor.modal';
     AliasesListerComponent,
     ProfilesComponent,
     ProfilesListerComponent,
+    ProfilesEditorModalComponent,
+    AliasesEditorModalComponent,
     ],
     imports: [
     BrowserModule,
