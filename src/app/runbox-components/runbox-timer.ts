7c3dbc7fa82d34610ecd32f288aef1552b0e7e84
Hernan Lopes <hernan604@runbox.com>
2020-01-16 21:27:20 +0100

    feat(runbox-timer): use ngOnInit instead of AfterViewInit and also accept a custom template

--- src/app/runbox-components/runbox-timer.ts
+++ src/app/runbox-components/runbox-timer.ts
@@ -61,19 +61,26 @@ import {RMM} from '../rmm';
     `],
     template: `
     <div class="app-runbox-timer" [ngClass]="css_class">
-        <div class="years" *ngIf="years">{{years}} years</div>
-        <div class="months" *ngIf="months">{{months}} months</div>
-        <div class="days" *ngIf="months">{{days}} days</div>
-        <div class="hours">{{hours}} hours</div>
-        <div class="minutes" >{{minutes}} minutes</div>
-        <div class="seconds" >{{seconds}} seconds</div>
+        <ng-container><span #ref><ng-content select="[custom_template]"></ng-content></span></ng-container>
+        <ng-container *ngIf="ref.childNodes.length == 0">
+            <div>
+              <div class="timeunit years" *ngIf="years">{{years}}</div>
+              <div class="timeunit months" *ngIf="months">{{months}}</div>
+              <div class="timeunit days" *ngIf="months">{{days}}</div>
+              <div class="timeunit hours">{{hours}}</div>
+              <div class="timeunit minutes" >{{minutes}}</div>
+              <div class="timeunit seconds" >{{seconds}}</div>
+            </div>
+        </ng-container>
     </div>
     `
 })
 
-export class RunboxTimerComponent  implements AfterViewInit {
+export class RunboxTimerComponent {
+  @ContentChild('custom_template', { static: false }) custom_template: ElementRef;
   @Input() future_date: any; // yyyy/mm/dd hh:mm:ss
   @Input() css_class: any; // cool_timer_css
+  @Input() child_timer: any = {years:0,months:0,days:0,hours:0,minutes:0,seconds:0};
   private dialog_ref: any;
 
   years: any;
@@ -88,10 +95,15 @@ export class RunboxTimerComponent  implements AfterViewInit {
     public dialog: MatDialog,
     public rmm: RMM,
     public snackBar: MatSnackBar,
+    public ref: ElementRef,
   ) {
   }
 
   ngAfterViewInit() {
+  
+  }
+
+  ngOnInit() {
       this.recalculate_date();
   }
 
@@ -126,6 +138,11 @@ export class RunboxTimerComponent  implements AfterViewInit {
       this.minutes = Math.floor(total_seconds/secs_per_minutes);
       total_seconds -= secs_per_minutes * this.minutes;
       this.seconds = total_seconds;
+
+      ['years','months','days','hours','minutes','seconds'].forEach((t)=>{
+        this.child_timer[t] = this[t];
+      });
+
      // console.log(` ${years} years, ${months} months, ${days} days, ${hours} hours, ${minutes} minutes, ${secs} seconds to go`)
   }
 }
