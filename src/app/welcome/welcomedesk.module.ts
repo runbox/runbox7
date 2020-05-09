00f729f16ace9180811d287402602fd51991aaee
Geir Thomas Andersen <andersen@runbox.com>
2020-05-09 19:20:10 +0200

    feat(welcome): Add content to Welcome Desk.

--- src/app/welcome/welcomedesk.module.ts
+++ src/app/welcome/welcomedesk.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeDeskComponent } from './welcomedesk.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [WelcomeDeskComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
   ]
 })
 export class WelcomeDeskModule { }
