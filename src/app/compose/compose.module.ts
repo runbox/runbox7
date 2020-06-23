478fa358a308ac97da4e05ef13b412687e275bee
Hernan Lopes <hernan604@runbox.com>
2020-06-23 21:53:16 +0200

    fix(compose): update angular deprecated recommendations

--- src/app/compose/compose.module.ts
+++ src/app/compose/compose.module.ts
@@ -24,7 +24,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
