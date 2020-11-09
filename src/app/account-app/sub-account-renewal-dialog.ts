e74b126163e66fe11a632d0262a98a092e7dcabc
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2020-11-09 16:24:30 +0100

    feat(account): Add a way to view subaccounts associated with a product
    Fixes GH-425.

---
+++ src/app/account-app/sub-account-renewal-dialog.ts
@@ -0,0 +1,52 @@
+// --------- BEGIN RUNBOX LICENSE ---------
+// Copyright (C) 2016-2020 Runbox Solutions AS (runbox.com).
+//
+// This file is part of Runbox 7.
+//
+// Runbox 7 is free software: You can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// Runbox 7 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
+// ---------- END RUNBOX LICENSE ----------
+
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-sub-account-renewal-dialog-component',
+    template: `
+<h1 mat-dialog-title> Sub-account product </h1>
+<div mat-dialog-content style="display: flex; flex-direction: column;">
+    <div>
+        <p> The following sub-accounts are associated with this product: </p>
+        <ol>
+            <li *ngFor="let username of product.subaccounts"> {{ username }} </li>
+        </ol>
+    </div>
+</div>
+<div mat-dialog-actions style="justify-content: space-between;">
+    <button mat-button (click)="dialogRef.close()"> Close </button>
+    <button mat-flat-button class="contentButton" (click)="dialogRef.close(true)">
+        Renew <mat-icon svgIcon="cart"></mat-icon>
+    </button>
+</div>
+    `,
+})
+export class SubAccountRenewalDialogComponent {
+    product: any;
+
+    constructor(
+        public dialogRef: MatDialogRef<SubAccountRenewalDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) {
+        this.product = data;
+    }
+}
