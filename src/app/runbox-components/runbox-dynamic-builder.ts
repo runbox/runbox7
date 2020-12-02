9df845ea580a3e3c593139ac1516811a48e7cef4
Hernan Lopes <hernan604@runbox.com>
2020-12-02 19:09:49 +0100

    feat(builder): runbox dynamic builder reserach

---
+++ src/app/runbox-components/runbox-dynamic-builder.ts
@@ -0,0 +1,143 @@
+// --------- BEGIN RUNBOX LICENSE ---------
+// Copyright (C) 2016-2020 Runbox Solutions AS (runbox.com).
+// 
+// This file is part of Runbox 7.
+// 
+// Runbox 7 is free software: You can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+// 
+// Runbox 7 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
+// ---------- END RUNBOX LICENSE ----------
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RMM } from '../rmm';
+
+@Component({
+    selector: 'app-runbox-dynamic-builder-component',
+    styles: [`
+    `],
+    template: `
+    <div class="app-runbox-dynamic-builder-component">
+        <div *ngIf="elem['rows']">
+            <div *ngFor="let row of elem.rows" style="">
+                <!-- loop in rows -- [attributes] used to bind values should have the same [attr-name] -->
+                <div [style]="'flex-direction: row; ' + row.style">
+                    <div *ngFor="let col of row.cols" [style]="'display: flex; flex-direction: column; ' + col.style">
+                        <div *ngIf="col.type === 'form'; else elseBlock">
+                            <!-- loop in form rows -->
+                            <form [class]="col.class">
+                                <!-- form -->
+                                <div
+                                    [style]="'display: flex; flex-direction: row; ' + row.style"
+                                    *ngFor="let row of col.rows"
+                                    [cols]="row.cols.length"
+                                >
+                                    <div
+                                        *ngFor="let col of row.cols"
+                                        [style]="'flex-direction: column; ' + col.style"
+                                        [class]="col.class"
+                                    >
+                                        <!-- recursive call -->
+                                        <app-runbox-dynamic-builder-component [elem]="col">
+                                        </app-runbox-dynamic-builder-component>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <ng-template #elseBlock>
+                            <div>
+                                <!-- recursive call -->
+                                <app-runbox-dynamic-builder-component [elem]="col">
+                                </app-runbox-dynamic-builder-component>
+                            </div>
+                        </ng-template>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div *ngIf="!elem['rows']">
+            <div *ngIf="elem.type === 'code'">
+                <pre
+                    [class]="elem.class"
+                    [style]="elem.style"
+                >
+                    <code [innerHTML]="elem.text"></code>
+                </pre>
+            </div>
+            <div *ngIf="elem.type === 'p'">
+                <p
+                    [class]="elem.class"
+                    [style]="elem.style"
+                >
+                    {{elem.text}}
+                </p>
+            </div>
+            <div *ngIf="elem.type === 'h1'">
+                <h1
+                    [class]="elem.class"
+                    [style]="elem.style"
+                >
+                    {{elem.text}}
+                </h1>
+            </div>
+            <div *ngIf="elem.type === 'h2'">
+                <h2
+                    [class]="elem.class"
+                    [style]="elem.style"
+                >
+                    {{elem.text}}
+                </h2>
+            </div>
+            <div *ngIf="elem.type === 'input'">
+                <!-- input -->
+                <mat-form-field [class]="elem.class">
+                    <mat-label *ngIf="elem.label">
+                        {{elem.label.text}}
+                    </mat-label>
+                    <input
+                        matInput
+                        [type]="elem.type"
+                        [placeholder]="elem.input.placeholder"
+                        [(ngModel)]="elem.value"
+                    >
+                </mat-form-field>
+                <mat-error *ngFor="let error of elem.errors" [innerHTML]="error"></mat-error>
+            </div>
+            <div *ngIf="elem.type === 'button'">
+                <!-- button -->
+                <button
+                    mat-raised-button
+                    (click)="elem.button.event && elem.button.event.click ? elem.button.event.click($event) : false"
+                    [class]="elem.class"
+                    [colmor]="elem.button.color || 'warn'"
+                    [disabled]="elem.button.disabled || false"
+                    [style]="elem.style"
+                >
+                        {{elem.button.text}}
+                </button>
+            </div>
+        </div>
+    </div>
+    `
+})
+
+export class RunboxDynamicBuilderComponent {
+  @Input() sidebar_opened = false;
+  @Input() elem = {};
+  constructor(public dialog: MatDialog,
+    public rmm: RMM,
+    public snackBar: MatSnackBar,
+  ) {
+  }
+
+}
+
---
+++ src/app/runbox-components/runbox-dynamic.ts
@@ -0,0 +1,294 @@
+// --------- BEGIN RUNBOX LICENSE ---------
+// Copyright (C) 2016-2020 Runbox Solutions AS (runbox.com).
+// 
+// This file is part of Runbox 7.
+// 
+// Runbox 7 is free software: You can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+// 
+// Runbox 7 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
+// ---------- END RUNBOX LICENSE ----------
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RMM } from '../rmm';
+
+@Component({
+    selector: 'app-runbox-dynamic',
+    styles: [`
+    `],
+    template: `
+    <div class="app-runbox-dynamic">
+        <app-runbox-dynamic-builder-component
+            [elem]="title"
+        >
+        </app-runbox-dynamic-builder-component>
+        <app-runbox-dynamic-builder-component
+            [elem]="multi_paragraphs"
+        >
+        </app-runbox-dynamic-builder-component>
+        <app-runbox-dynamic-builder-component
+            [elem]="my_form"
+        >
+        </app-runbox-dynamic-builder-component>
+    </div>
+    `
+})
+
+export class RunboxDynamicComponent {
+  @Input() sidebar_opened = false;
+  public test;
+  public my_form;
+  public title;
+  public multi_paragraphs;
+  public result;
+  constructor(public dialog: MatDialog,
+    public rmm: RMM,
+    public snackBar: MatSnackBar,
+  ) {
+    this.build_my_form();
+  }
+
+  build_my_form () {
+      let firstname = {
+        style: '',
+        class: '',
+        type: 'input',
+        //value: 'Hernan',
+        label: {
+            text: 'Firstname:'
+        },
+        input: {
+            placeholder: 'Enter your firstname',
+            type: 'text',
+        },
+      };
+
+      firstname['validate'] = () => {
+          firstname['errors'] = [];
+          if ( ! firstname['value'] ) {
+            firstname['errors'].push('Firstname is <strong>required</strong>');
+          }
+          return firstname['errors'].length ? false : true;
+      };
+
+      let lastname = {
+        style: '',
+        class: '',
+        type: 'input',
+        //value: 'Lopes',
+        label: {
+            text: 'Lastname:'
+        },
+        input: {
+            placeholder: 'Enter your last',
+            type: 'text',
+        },
+      };
+
+      lastname['validate'] = () => {
+          lastname['errors'] = [];
+          if ( ! lastname['value'] ) {
+            lastname['errors'].push('Lastname is <strong>required</strong>');
+          }
+          return lastname['errors'].length ? false : true;
+      };
+
+      let email = {
+        style: '',
+        class: '',
+        type: 'input',
+        label: {
+            text: 'Email:'
+        },
+        input: {
+            placeholder: 'Enter your email. ie. name@example.com',
+            type: 'email',
+        },
+      };
+
+      email['validate'] = () => {
+          email['errors'] = [];
+          if ( ! email['value'] ) {
+            email['errors'].push('Email is <strong>required</strong>');
+          }
+          else {
+            if ( ! email['value'].match(/.+@.+\..+/g) ) {
+                email['errors'].push('Invalid format. ie. name@domain.com')
+            }
+          }
+          return email['errors'].length ? false : true;
+      };
+
+      this.title = {
+        style: '',
+        class: 'mat-h1',
+        type: 'h1',
+        text: 'Runbox7 Dynamic Builder!',
+      };
+
+      let btn_subscribe = {
+        style: '',
+        class: '',
+        type: 'button',
+        button: {
+            color: 'primary',
+            text: 'Subscribe',
+            disabled: false,
+            'mat-button': true,
+            event: {
+                click: function () {
+                  let has_error = false;
+                  if ( ! firstname['validate']() ) { has_error = true }
+                  if ( ! lastname['validate']() ) { has_error = true }
+                  if ( ! email['validate']() ) { has_error = true }
+                  if ( ! has_error ) {
+                      this.result = {
+                        firstname: firstname['value'],
+                        lastname: lastname['value'],
+                        email: email['value'],
+                      };
+                      alert('Form validated. Make that POST with: \n'+JSON.stringify(this.result))
+                  }
+                },
+            }
+        }
+      };
+
+      let btn_load_values = {
+        style: '',
+        class: '',
+        type: 'button',
+        button: {
+            color: 'warn',
+            text: 'Load values',
+            //disabled: true,
+            event: {
+                click: (e: any, col) => {
+                    console.log(e.target);
+                    console.log('col', col);
+                    let values = {};
+                },
+            }
+        }
+      };
+
+      let paragraph_additional_notes = {
+          style: '',
+          class: '',
+          type: 'p',
+          text:'This paragraph is an example of some notes. It could be used to describe the form fields or some other important property.'
+      };
+
+      let form_row_notes = {
+        style: '' ,
+        class: '',
+        cols: [ paragraph_additional_notes, ],
+      };
+
+      let form_row_names = {
+        style: '' ,
+        class: '',
+        cols: [ firstname, lastname, ],
+      };
+
+      let form_row_email = {
+        style: '' ,
+        class: '',
+        cols: [ email, ],
+      };
+
+      let form_row_2 = {
+        style: '' ,
+        class: '',
+        cols: [ btn_subscribe, btn_load_values ],
+      };
+
+      let form = {
+        style: '',
+        type: 'form',
+        class: '',
+        rows: [
+          form_row_names,
+          form_row_notes,
+          form_row_email,
+          form_row_2,
+        ]
+      };
+
+      btn_load_values.button.event.click = ( e: any ) => {
+        firstname['value'] = "Hernan";
+        lastname['value'] = "Lopes";
+        email['value'] = "hernan604@runbox.com";
+      }
+
+      let row = {
+        style: '' ,
+        class: '',
+        cols: [ form ],
+      };
+
+      this.my_form = {
+        type: 'form',
+            rows: [
+              row
+            ]
+      };
+
+      this.multi_paragraphs = {
+        rows: [
+            {
+                style: '',
+                class: '',
+                cols: [
+                    {
+                    type: 'p',
+                    text: 'Welcome to the runbox dynamic builder. This builder is a possible recursive html generator. The objective of this builder is to create standard html across the runbox angular webapp. The idea is to let the recursive html generator do its job of building the html, and let the frontend developers focus on business logic instead. With this, the developer will not need to type angular html.'
+                    },
+                ]
+            },
+            {
+                style: '',
+                class: '',
+                cols: [
+                    {
+                    type: 'p',
+                    text: 'It can transform the following structure:'
+                    },
+                ]
+            },
+            {
+                style: '',
+                class: '',
+                cols: [
+                    {
+                    type: 'code',
+                    text: JSON.stringify(this.my_form, null, 2)
+                    },
+                ]
+            },
+            {
+                style: '',
+                class: '',
+                cols: [
+                    {
+                    type: 'p',
+                    text:'into the following form:'
+                    },
+                ]
+            },
+
+        ]
+      };
+  }
+
+}
+
