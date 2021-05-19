b9e40dcb3516c6aadba7697abfeccc83a7b26ca8
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2021-05-19 14:30:49 +0200

    refactor(contacts): simplify formarray editors
    I still hate this design, but this is at least a little saner now.

--- src/app/contacts-app/contact-details/formarray-editor.component.ts
+++ src/app/contacts-app/contact-details/formarray-editor.component.ts
@@ -27,7 +27,6 @@ import { FormArray, FormGroup } from '@angular/forms';
 export class FormArrayEditorComponent implements OnInit {
     @Input() inputFG: FormGroup;
     @Input() faName:  string;
-    @Input() layout:  string; // 'simple' or 'complex'
 
     @Input() hidden   = true;
 
@@ -35,9 +34,6 @@ export class FormArrayEditorComponent implements OnInit {
     @Input() valuePlaceholder:  string;
     @Input() deleteDescription: string;
     @Input() addNewDescription: string;
-
-    @Input() actionIcon:        string;
-    @Input() actionDescription: string;
 
     @Output() newElementClicked = new EventEmitter<any>();
 
