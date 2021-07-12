89752d802e7bfc86782f38ea803951404d77a87b
Olimpia Kwiecień <kwiecien@zoho.com>
2021-07-12 22:13:32 +0200

    feat(account-security): Add possibility to change Password

--- src/app/account-security/account.security.module.ts
+++ src/app/account-security/account.security.module.ts
@@ -51,11 +51,13 @@ import { ManageServicesComponent } from './manage-services.component';
 import { AppPasswordsComponent } from './app-passwords.component';
 import { LastLoginsComponent } from './last-logins.component';
 import { SessionsComponent } from './sessions.component';
+import { AccountPasswordComponent } from './account-password.component';
 
 @NgModule({
     declarations: [
       ModalUnlockcodeComponent,
       ModalPasswordComponent,
+      AccountPasswordComponent,
       TwoFactorAuthenticationComponent,
       ManageServicesComponent,
       AppPasswordsComponent,
