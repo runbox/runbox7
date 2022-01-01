f48eb8253cb9a0263423a2d814df98e2d3cdce5e
Geir Thomas Andersen <andersen@runbox.com>
2022-01-01 18:53:51 +0100

    feat(payment): Re-enable tests.

--- e2e/cypress/integration/ordering.ts
+++ e2e/cypress/integration/ordering.ts
@@ -16,29 +16,29 @@ describe('Ordering products', () => {
             .and('contain', '13.37 EUR');
     });
 
-    // it('can select a payment method and pay', () => {
-    //     cy.contains('mat-expansion-panel-header', 'Other payment methods').click();
-    //     cy.get('button#payDirectly').click();
-    //     cy.url().should('include', '/account/receipt');
-    // });
+    it('can select a payment method and pay', () => {
+        cy.contains('mat-expansion-panel-header', 'Other payment methods').click();
+        cy.get('button#payDirectly').click();
+        cy.url().should('include', '/account/receipt');
+    });
 
-    // it('can order product twice to increase quantity', () => {
-    //     cy.visit('/account/plans');
+    it('can order product twice to increase quantity', () => {
+        cy.visit('/account/plans');
 
-    //     cy.get('#shoppingCartButton').should('not.exist');
+        cy.get('#shoppingCartButton').should('not.exist');
 
-    //     cy.get('.productGrid .contentButton').contains('Purchase').click();
-    //     cy.get('#shoppingCartButton').should('be.visible');
-    //     cy.get('#shoppingCartButton .mat-badge-content').should('contain', '1');
+        cy.get('.productGrid .contentButton').contains('Purchase').click();
+        cy.get('#shoppingCartButton').should('be.visible');
+        cy.get('#shoppingCartButton .mat-badge-content').should('contain', '1');
 
-    //     cy.get('.productGrid .contentButton').contains('Purchase').click();
-    //     cy.get('#shoppingCartButton .mat-badge-content').should('contain', '1');
+        cy.get('.productGrid .contentButton').contains('Purchase').click();
+        cy.get('#shoppingCartButton .mat-badge-content').should('contain', '1');
 
-    //     cy.get('#shoppingCartButton').click();
+        cy.get('#shoppingCartButton').click();
 
-    //     cy.get('tr.mat-row td:nth-of-type(1)').should('contain', 'Runbox Addon');
-    //     cy.get('tr.mat-row td:nth-of-type(2)').should('contain', '2');
-    //     cy.get('tr.mat-row td button').click();
-    //     cy.contains('shopping cart is currently empty');
-    // });
+        cy.get('tr.mat-row td:nth-of-type(1)').should('contain', 'Runbox Addon');
+        cy.get('tr.mat-row td:nth-of-type(2)').should('contain', '2');
+        cy.get('tr.mat-row td button').click();
+        cy.contains('shopping cart is currently empty');
+    });
 });
