fbcc2626f6765ab2ee020e87251c5f58bb39073a
Jess Robinson <j.robinson@shadowcat.co.uk>
2022-06-08 15:20:46 +0000

    test(all): Ensure we wait long enough in e2e tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -422,8 +422,8 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
     this.router.events.pipe(
       filter(e => e instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
-      if (this.router.url !== '/compose?new=true'
-         && this.router.url !== '/compose') {
+      if (this.router.url !== '/compose?new=true') {
+        // && this.router.url !== '/compose') {
         this.draftDeskService.previousPageUrl = this.router.url;
       }
       this.composeSelected = this.router.url === '/compose?new=true';
