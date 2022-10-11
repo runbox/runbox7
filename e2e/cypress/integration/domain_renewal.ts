5bcb46d11f16a580d9de4ba64acec9d8716650be
Geir Thomas Andersen <andersen@runbox.com>
2022-10-11 10:22:56 +0200

    fix(test): Update tests.

--- e2e/cypress/integration/domain_renewal.ts
+++ e2e/cypress/integration/domain_renewal.ts
@@ -4,7 +4,7 @@ describe('Renewing domains', () => {
     it('can renew a domain through domreg', () => {
         cy.visit('/account/subscriptions');
 
-        cy.get('tr:contains("INC 10 year(s)")').get('.contentButton').contains('Renew').click();
+        cy.get('tr:contains("INC 10 year(s)")').get('#renewButton').contains('Renew').click();
         cy.url().should('include', '/domainregistration?renew_domain=monsters.inc');
     });
 });
