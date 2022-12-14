067ec0ff52877a9eb690429bd4fbf7e84a18e36e
Geir Thomas Andersen <andersen@runbox.com>
2022-12-14 12:43:24 +0100

    test(Contacts): Update test.

--- e2e/cypress/integration/contacts.ts
+++ e2e/cypress/integration/contacts.ts
@@ -3,7 +3,7 @@
 describe('Display contact details', () => {
     it('Should display a clickable contact on a list', () => {
         cy.visit('/contacts');
-        cy.contains('Welcome to Runbox 7 Contacts');
+        cy.contains('Runbox 7 Contacts');
         cy.contains('Patrick Postcode').click();
         cy.url().should('include', 'id-mr-postcode');
         cy.get('input[data-placeholder="Company"').should('have.value', 'Post Office #42');
