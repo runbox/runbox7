c38f460b9003baaeb426f63f033570aeb0ebd73e
Antonis Kalou <kalouantonis@protonmail.com>
2023-01-19 13:16:37 +0200

    build(deps): Upgrade cypress to 9.7
    Remove deprecated cypress-file-upload
    Update cypress-terminal-report to v2
    Update @cypress/webpack-preprocessor to latest

--- e2e/cypress/integration/calendar-import.ts
+++ e2e/cypress/integration/calendar-import.ts
@@ -4,7 +4,7 @@ describe('Import calendar event', () => {
     it('Should display import preview', () => {
         cy.visit('/calendar');
 
-        cy.get('input[type=file]').attachFile({ filePath: 'event.ics' });
+        cy.get('input[type=file]').selectFile({ contents: 'cypress/fixtures/event.ics' });
         cy.contains('Pick up my car');
     });
 });
