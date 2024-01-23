8aeeee3f04c60af86c732ceb9d2f1dad89bfa2a7
Jess Robinson <j.robinson@shadowcat.co.uk>
2024-01-23 09:58:06 +0000

    feat(install): Replaces index dialog prompt with sidebar buttons
    Fixes #1509

--- e2e/cypress/integration/search.ts
+++ e2e/cypress/integration/search.ts
@@ -3,7 +3,6 @@
 describe('Search', () => {
     it('should display multiple search field panel', () => {
         cy.visit('/');
-        cy.closeWelcomeDialog();
         cy.get('mat-toolbar mat-form-field button').click();
         cy.get('input[data-placeholder=Subject]').type('testsubject');
         cy.get('mat-toolbar input[data-placeholder="Start typing to search messages"]')
