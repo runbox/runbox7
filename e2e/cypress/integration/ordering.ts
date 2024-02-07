33da9db6fc9f47c011c6cdc951239ed9772dcdfb
Jess Robinson <j.robinson@shadowcat.co.uk>
2024-02-07 13:01:28 +0000

    test(ordering): Ensure quantity increase test uses add-on product
    Tidied up code displaying subscriptions upgrade/downgrade buttons

--- e2e/cypress/integration/ordering.ts
+++ e2e/cypress/integration/ordering.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Ordering products', { testIsolation: false }, () => {
+describe('Ordering products', () => {
     it('can place an order', () => {
         localStorage.removeItem('221:shoppingCart');
         cy.intercept('/rest/v1/account_product/available').as('availableProducts');
@@ -9,17 +9,13 @@ describe('Ordering products', { testIsolation: false }, () => {
         cy.wait('@availableProducts', {'timeout':10000});
         cy.get('.productGrid .purchaseButton').contains('Purchase').click();
         cy.get('#shoppingCartButton').should('be.visible');
-    });
 
-    it('cart contains the purchased product', () => {
         cy.get('#shoppingCartButton').click();
         cy.url().should('include', '/account/cart');
         cy.get('.mat-table tbody tr')
             .and('contain', 'Runbox Test')
             .and('contain', '13.37 EUR');
-    });
 
-    it('can select a payment method and pay', () => {
         cy.contains('mat-expansion-panel-header', 'Other payment methods').click();
         cy.get('button#payDirectly').click();
         cy.url().should('include', '/account/receipt');
@@ -30,11 +26,11 @@ describe('Ordering products', { testIsolation: false }, () => {
 
         cy.get('#shoppingCartButton').should('not.exist');
 
-        cy.get('.productGrid .purchaseButton').contains('Purchase').click();
+        cy.get('.productGrid .purchaseButton').contains('Add product').click();
         cy.get('#shoppingCartButton').should('be.visible');
         cy.get('#shoppingCartButton .mat-badge-content').should('contain', '1');
 
-        cy.get('.productGrid .purchaseButton').contains('Purchase').click();
+        cy.get('.productGrid .purchaseButton').contains('Add product').click();
         cy.get('#shoppingCartButton .mat-badge-content').should('contain', '1');
 
         cy.get('#shoppingCartButton').click();
