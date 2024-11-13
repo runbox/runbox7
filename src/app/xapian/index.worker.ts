2674e2a0493feef893df8b73a1982c04e1bb99c4
Bas <b.huis@shadowcat.co.uk>
2024-11-13 14:19:00 +0200

    feat(worker): Add sentry to xapian worker scope

--- src/app/xapian/index.worker.ts
+++ src/app/xapian/index.worker.ts
@@ -1,23 +1,26 @@
 // --------- BEGIN RUNBOX LICENSE ---------
 // Copyright (C) 2016-2022 Runbox Solutions AS (runbox.com).
-// 
+//
 // This file is part of Runbox 7.
-// 
+//
 // Runbox 7 is free software: You can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
-// 
+//
 // Runbox 7 is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
 // ---------- END RUNBOX LICENSE ----------
 
 /// <reference lib="webworker.importscripts" />
+
+import '../sentry';
+
 import { Observer, Observable, of, from, AsyncSubject } from 'rxjs';
 import { mergeMap, map, filter, catchError, tap, take, bufferCount } from 'rxjs/operators';
 
@@ -250,7 +253,7 @@ class SearchIndexService {
     }
 
     this.localSearchActivated = false;
-    
+
     return new Observable((observer) => {
       console.log('Worker: Closing xapian database');
       if (this.api) {
