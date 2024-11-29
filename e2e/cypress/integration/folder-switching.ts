1449517335074b14f777d28a1da9111bb2a7a6ef
Bas <b.huis@shadowcat.co.uk>
2024-11-29 12:52:01 +0000

    feat(message-list): Implement virtual scroll table
    - Improves on the less accessible canvastable.
    - Introduces multi select using ctrl and shift.
    - Reduces memory footprint by a good margin.

--- e2e/cypress/integration/folder-switching.ts
+++ e2e/cypress/integration/folder-switching.ts
@@ -1,19 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Switching between folders (and not-folders)', () => {
-
     function goToInbox() {
         cy.get('rmm-folderlist mat-tree-node:contains(Inbox)', {'timeout':10000}).click();
         cy.url().should('match', /\/(#Inbox)?$/);
         cy.get('rmm-folderlist mat-tree-node:contains(Inbox)').should('have.class', 'selectedFolder');
     }
 
-  it('can switch from welcome to inbox', () => {
-    localStorage.setItem('221:localSearchPromptDisplayed', JSON.stringify('true'));
-    localStorage.setItem('221:localSearchPromptDisplayed', JSON.stringify('true'));
+    it('can switch from welcome to inbox', () => {
+        localStorage.setItem('221:localSearchPromptDisplayed', JSON.stringify('true'));
+        localStorage.setItem('221:localSearchPromptDisplayed', JSON.stringify('true'));
 
-    // start of on /welcome, like a fresh new user
-    cy.visit('/welcome');
+        // start of on /welcome, like a fresh new user
+        cy.visit('/welcome');
 
         // should be able to switch to inbox...
         goToInbox();
