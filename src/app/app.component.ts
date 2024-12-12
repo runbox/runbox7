9dc3b3f05ac35eab26e08007aa57ddc381d21881
Bas <b.huis@shadowcat.co.uk>
2024-12-12 17:09:31 +0000

    Do not deselect for certain actions. Fix bulk action when no item is checked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,7 @@ import { StorageService } from './storage.service';
 import { SearchMessageDisplay } from './xapian/searchmessagedisplay';
 import { UsageReportsService } from './common/usage-reports.service';
 import { objectEqualWithKeys } from './common/util';
-import { SelectionModel } from '@angular/cdk/collections';
+import { FilterSelectionModel } from './models/filter-selection-model';
 
 const fetchedSymbol = Symbol('fetched')
 
@@ -94,17 +94,19 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
   rows = [];
   lastCheckedRow = null;
   scrollToIndex: number = 0;
-  rowSelectionModel = new SelectionModel(
+  rowSelectionModel = new FilterSelectionModel(
     false,
     [],
     false,
-    (a, b) => a?.id === b?.id // Custom comparison based on `id`
+    messagesEqual,
+    hasId
   );
-  rowsSelectionModel = new SelectionModel(
+  rowsSelectionModel = new FilterSelectionModel(
     true,
     [],
     false,
-    (a, b) => a?.id === b?.id // Custom comparison based on `id`
+    messagesEqual,
+    hasId
   );
 
   lastSearchText = '';
@@ -123,10 +125,7 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
   localSearchIndexPrompted = false;
   offerInitialLocalIndex = false;
 
-  dragEvent: null | {
-    event: DragEvent,
-    selected: Array<MessageInfo>
-  };
+  dragEvent: DragEvent | null = null
 
   indexDocCount = 0;
 
@@ -191,6 +190,8 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
 
   morelistbuttonindex = 7;
   renderedRange = {start: 0, end: 0}; // First ten messages.
+
+  dateWidth = 10;
 
   constructor(
     public searchService: SearchService,
@@ -627,7 +628,7 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
       true, spamFolderName
     ).toPromise();
 
-    const messageIds = messageLists.map(msg => msg.id);
+    const messageIds = messageLists.map(idValue);
     this.messageActionsHandler.updateMessages({
       messageIds: messageIds,
       updateLocal: (msgIds: number[]) => this.messagelistservice.moveMessages(msgIds, this.messagelistservice.trashFolderName),
@@ -768,7 +769,6 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
             new MessageFlagChange(id, status, null)
           );
         });
-        this.clearSelection();
         if (this.singlemailviewer && messageIds.find((id) => id === this.singlemailviewer.messageId)) {
           this.singlemailviewer.mailObj.seen_flag = status ? 1 : 0;
         }
@@ -801,7 +801,6 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
             new MessageFlagChange(id, null, status)
           );
         });
-        this.clearSelection();
         if (this.singlemailviewer && messageIds.find((id) => id === this.singlemailviewer.messageId)) {
           this.singlemailviewer.mailObj.flagged_flag = status ? 1 : 0;
         }
@@ -1107,8 +1106,21 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
     }
   }
 
+  onMessagesDragStart(event: DragEvent, row) {
+
+    // If no messages are selected we'll select the current message
+    if (this.rowsSelectionModel.isEmpty()) {
+      this.rowsSelectionModel.select(row)
+    }
+
+    // Remove the default image
+    event.dataTransfer?.setDragImage(new Image(), 0, 0); // Set an empty image
+
+    this.dragEvent = event
+  }
+
   dropToFolder(folderId): void {
-    const messageIds = Array.from(this.dragEvent.selected).map(x => x.id)
+    const messageIds = this.selectedMessageIds
 
     this.messageActionsHandler.updateMessages({
       messageIds: messageIds,
@@ -1460,7 +1472,9 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
   }
 
   get selectedMessageIds() {
-    return this.rowsSelectionModel.selected.reduce((acc, x) => x?.id ? acc.concat(x.id): acc, [])
+    return this.rowsSelectionModel.isEmpty()
+      ? this.rowSelectionModel.selected.map(idValue)
+      : this.rowsSelectionModel.selected.map(idValue)
   }
 
   updateRows() {
@@ -1571,10 +1585,6 @@ export class AppComponent implements OnInit, AfterViewInit, CanvasTableSelectLis
     return this.rowsSelectionModel.selected.length === this.rows.length
   }
 
-  onSelectionDragStarted(event) {
-    this.dragEvent = event;
-  }
-
   @HostListener('document:dragend', ['$event'])
   onDragEnded() {
     delete this.dragEvent;
@@ -1629,3 +1639,7 @@ function mapOverIndexes<T>(
 
   return result;
 }
+
+const idValue = x => x.id
+const messagesEqual = (a, b) => a?.id === b?.id
+const hasId = (x) => Boolean(x?.id)
--- src/app/directives/resizer.directive.ts
+++ src/app/directives/resizer.directive.ts
@@ -1,23 +1,23 @@
 // --------- BEGIN RUNBOX LICENSE ---------
 // Copyright (C) 2016-2018 Runbox Solutions AS (runbox.com).
-// 
+//
 // This file is part of Runbox 7.
-// 
+//
 // Runbox 7 is free software: You can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
-// 
+//
 // Runbox 7 is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
 // ---------- END RUNBOX LICENSE ----------
 
-import { Directive, ElementRef, OnInit, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input, Renderer2, Output, EventEmitter } from '@angular/core';
 
 @Directive({
     selector: '[appResizable]' // Attribute selector
@@ -26,6 +26,13 @@ export class ResizerDirective implements OnInit {
     @Input() resizableGrabWidth = 8;
     @Input() resizableMinWidth = 10;
     @Input() position = 'start';
+
+    @Output() resizeStart = new EventEmitter<{ startWidth: number, position: string }>();  // Emit initial width and position
+    @Output() resizing = new EventEmitter<number>();   // Emit current width during resizing
+    @Output() resizeEnd = new EventEmitter<{ finalWidth: number, position: string }>();    // Emit final width and position
+
+    @Output() valueChange = new EventEmitter<number>();  // Custom event to emit width changes
+    @Input() value: number;  // Input to set the width (can be bound to parent component)
 
     dragging = false;
 
@@ -37,14 +44,20 @@ export class ResizerDirective implements OnInit {
             if (!this.dragging) {
                 return;
             }
+            let newWidth: number;
+
             if (this.position === 'end') {
-                const newWidth = Math.max(this.resizableMinWidth,
-                            ((el.nativeElement.offsetLeft + el.nativeElement.offsetWidth) - evt.clientX)) ;
+                newWidth = Math.max(this.resizableMinWidth,
+                                    (el.nativeElement.offsetLeft + el.nativeElement.offsetWidth) - evt.clientX);
                 el.nativeElement.style.width = newWidth + 'px';
             } else {
-                const newWidth = Math.max(this.resizableMinWidth, (evt.clientX - el.nativeElement.offsetLeft)) ;
+                newWidth = Math.max(this.resizableMinWidth, evt.clientX - el.nativeElement.offsetLeft);
                 el.nativeElement.style.width = newWidth + 'px';
             }
+
+            this.resizing.emit(newWidth);  // Emit during resizing
+            this.value = newWidth;  // Update local value
+            this.valueChange.emit(newWidth);  // Emit the change to parent component
         };
 
         const mouseUp = (evt) => {
@@ -52,32 +65,33 @@ export class ResizerDirective implements OnInit {
                 return;
             }
             this.dragging = false;
-     //       console.log(" REMOVE DRAGGER ");
-            evt.preventDefault();
+            this.resizeEnd.emit({
+                finalWidth: el.nativeElement.offsetWidth,  // Emit final width and position when resizing ends
+                position: this.position
+            });
         };
 
         const mouseDown = (evt) => {
-    //        console.log("Mouse down");
             if (this.inDragRegion(evt)) {
                 this.dragging = true;
-      //          console.log("IN DRAG REGION");
                 evt.preventDefault();
+                this.resizeStart.emit({
+                    startWidth: el.nativeElement.offsetWidth,  // Emit initial width and position when resizing starts
+                    position: this.position
+                });
             }
         };
 
         this.renderer.listen('window', 'mousemove', mouseDrag);
         this.renderer.listen('window', 'mouseup', mouseUp);
-
         el.nativeElement.addEventListener('mousedown', mouseDown, true);
 
         el.nativeElement.addEventListener('mousemove', (evt) => {
-
             if (this.inDragRegion(evt) || this.dragging) {
                 el.nativeElement.style.cursor = 'col-resize';
             } else {
                 el.nativeElement.style.cursor = 'default';
             }
-            //   if (evt)
         }, true);
 
         this.renderer.listen('window', 'touchmove',
@@ -88,34 +102,34 @@ export class ResizerDirective implements OnInit {
         );
         this.renderer.listen('window', 'touchend', mouseUp);
         el.nativeElement.addEventListener('touchstart',
-                (evt: TouchEvent) =>  mouseDown(Object.assign(evt, {
-                    clientX: evt.targetTouches[0].clientX,
-                    clientY: evt.targetTouches[0].clientY
+            (evt: TouchEvent) => mouseDown(Object.assign(evt, {
+                clientX: evt.targetTouches[0].clientX,
+                clientY: evt.targetTouches[0].clientY
             }))
         );
     }
 
     ngOnInit(): void {
+        this.renderer.addClass(this.el.nativeElement, 'resizable');
+
         if (this.position !== 'end') {
             this.el.nativeElement.style['border-right'] = this.resizableGrabWidth + 'px solid darkgrey';
         } else {
             this.el.nativeElement.style['border-left'] = this.resizableGrabWidth + 'px solid darkgrey';
         }
-        console.log('Applied appresizable');
-    }
 
-    inDragRegion(evt) {
-        if (this.position === 'end') {
-            const rect = this.el.nativeElement.getBoundingClientRect();
-            const dragX1 = (rect.left );
-
-            return  evt.clientX < dragX1 + this.resizableGrabWidth;
-        } else {
-            const rect = this.el.nativeElement.getBoundingClientRect();
-            const dragX1 = (rect.left + rect.width);
-
-            return  evt.clientX > dragX1 - this.resizableGrabWidth;
+        // Set initial width if the value is provided from the parent component
+        if (this.value) {
+            this.el.nativeElement.style.width = this.value + 'px';
         }
     }
 
+    inDragRegion(evt) {
+        const rect = this.el.nativeElement.getBoundingClientRect();
+        const dragX1 = this.position === 'end' ? rect.left : rect.left + rect.width;
+
+        return this.position === 'end'
+            ? evt.clientX < dragX1 + this.resizableGrabWidth
+            : evt.clientX > dragX1 - this.resizableGrabWidth;
+    }
 }
