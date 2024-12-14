362e5217d297a181c5bafc586b447a8c69d9dc4d
Bas <b.huis@shadowcat.co.uk>
2024-12-14 00:26:10 +0000

    Enable ordering of columns on th click

--- src/app/models/filter-selection-model.ts
+++ src/app/models/filter-selection-model.ts
@@ -1,8 +1,8 @@
 import { SelectionModel } from '@angular/cdk/collections';
 
 export class FilterSelectionModel<T> extends SelectionModel<T> {
-  constructor(multiple: boolean, initialValues: T[], allowMultiSelect: boolean, comparator: (a: T, b: T) => boolean, predicate: (a) => boolean) {
-    super(multiple, initialValues, allowMultiSelect, comparator);
+  constructor(multiple: boolean, initialValues: T[], emitChanges: boolean, compareWith: (a: T, b: T) => boolean, predicate: (a) => boolean) {
+    super(multiple, initialValues, emitChanges, compareWith);
 
     return new Proxy(this, {
       get(target, prop) {
