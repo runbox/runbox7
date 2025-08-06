3d313d0835a740c2f22f46c8ab7615affb3f4465
Jess Robinson <j.robinson@shadowcat.co.uk>
2025-08-06 15:04:26 +0000

    fix(stripe): Allow dialog to scroll (make Submit reachable)

--- src/app/account-app/account-app.module.ts
+++ src/app/account-app/account-app.module.ts
@@ -58,7 +58,7 @@ import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/lega
 import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+import { MatDialogModule as MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { MatIconModule } from '@angular/material/icon';
