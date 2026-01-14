10c9eda6786e01fb764a3cf298778a8d4027434b
Finn <toyou1995@gmail.com>
2026-01-14 00:57:02 +0000

    test(welcome): add MatIconTestingModule to fix icon errors in tests

--- src/app/welcome/welcomedesk.component.spec.ts
+++ src/app/welcome/welcomedesk.component.spec.ts
@@ -1,18 +1,18 @@
 // --------- BEGIN RUNBOX LICENSE ---------
 // Copyright (C) 2016-2018 Runbox Solutions AS (runbox.com).
-// 
+//
 // This file is part of Runbox 7.
-// 
+//
 // Runbox 7 is free software: You can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
-// 
+//
 // Runbox 7 is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
 // ---------- END RUNBOX LICENSE ----------
@@ -23,6 +23,7 @@ import { WelcomeDeskComponent } from './welcomedesk.component';
 import { ActivatedRoute, Router, convertToParamMap } from '@angular/router';
 import { of } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { MatCardModule } from '@angular/material/card';
 
 describe('WelcomeDeskComponent', () => {
@@ -34,6 +35,7 @@ describe('WelcomeDeskComponent', () => {
       declarations: [ WelcomeDeskComponent ],
       imports: [
         MatIconModule,
+        MatIconTestingModule,
         MatCardModule,
       ],
       providers:
