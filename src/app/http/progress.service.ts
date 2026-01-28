796fecff05be73e547a4d217d2a4a776a8389f6f
Finn <toyou1995@gmail.com>
2026-01-28 02:57:16 +0000

    refactor(cleanup): remove unused code and constants

--- src/app/http/progress.service.ts
+++ src/app/http/progress.service.ts
@@ -1,26 +1,30 @@
 // --------- BEGIN RUNBOX LICENSE ---------
 // Copyright (C) 2016-2018 Runbox Solutions AS (runbox.com).
-// 
+//
 // This file is part of Runbox 7.
-// 
+//
 // Runbox 7 is free software: You can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
-// 
+//
 // Runbox 7 is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
 // ---------- END RUNBOX LICENSE ----------
 
 import {Injectable} from '@angular/core';
-import {Subject, BehaviorSubject} from 'rxjs';
+import {Subject, BehaviorSubject, Observable} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 @Injectable()
 export class ProgressService {
   httpRequestInProgress: Subject<boolean> = new BehaviorSubject(false);
+
+  // Delayed version to avoid ExpressionChangedAfterItHasBeenCheckedError in templates
+  httpRequestInProgress$: Observable<boolean> = this.httpRequestInProgress.pipe(delay(0));
 }
