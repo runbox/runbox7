6cb639ad2140ba90c05a6df8fedd78842a31247c
kei <keletrh@gmail.com>
2026-06-11 08:31:03 +0200

    build(test): configure karma browsers from env

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,17 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const DEFAULT_BROWSERS = ['Firefox'];
+
+function configuredBrowsers(envValue) {
+  if (!envValue) {
+    return DEFAULT_BROWSERS;
+  }
+
+  const browsers = envValue.split(',').map((browser) => browser.trim()).filter(Boolean);
+  return browsers.length ? browsers : DEFAULT_BROWSERS;
+}
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,7 +38,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: configuredBrowsers(process.env.KARMA_BROWSERS),
     singleRun: false
   });
 };
+
+module.exports.configuredBrowsers = configuredBrowsers;
